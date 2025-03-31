4e57b732539fb01bff4c71f76288995731f1f1e7
Marek Bełz <belzmarek@gmail.com>
2025-03-31 11:00:03 +0000

    task 01 done

--- 01/app.js
+++ 01/app.js
@@ -7,11 +7,54 @@ class App extends React.Component {
     state = {
         counter: 0,
     }
+
+    componentDidMount() {
+        console.log('componentDidMount');
+        this.intervalId = setInterval(() => {
+            this.setState((prevState) => ({
+                counter: prevState.counter + 1,
+            }));
+        }, 5000);
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log('componentDidUpdate', { prevProps, prevState });
+    }
+
+    componentWillUnmount() {
+        console.log('componentWillUnmount');
+        clearInterval(this.intervalId);
+    }
+
+
     render() {
         console.log('render');
 
-        return <h1>{ this.state.counter }</h1>
+        return <h1>Licznik: { this.state.counter }</h1>
     }
 }
 
-root.render(<App/>);
+class Remove extends React.Component {
+    state = {
+        showApp: true, // Kontroluje, czy komponent App jest renderowany
+    };
+
+    toggleApp = () => {
+        this.setState((prevState) => ({
+            showApp: !prevState.showApp, // Przełącz widoczność komponentu App
+        }));
+    };
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.toggleApp}>
+                    {this.state.showApp ? 'Odinstaluj komponent' : 'Zainstaluj komponent'}
+                </button>
+                {this.state.showApp && <App />}
+            </div>
+        );
+    }
+}
+
+root.render(<Remove/>);
